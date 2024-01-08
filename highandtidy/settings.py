c2dfafd953931f890b9d476aa35d9e0320aa753c
Michael M. <mm@mm-ul.xyz>
2024-01-08 18:26:11 +0000

    Adding some settings

--- highandtidy/settings.py
+++ highandtidy/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'highandtidy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'highandtidy_initialtest20240107',
+        'USER': 'michael',
+        'PASSWORD': 'michael',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
