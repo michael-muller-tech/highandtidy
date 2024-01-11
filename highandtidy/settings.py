39f33cbfe308e8c6d0aaffc0aa88c8e3f9a74831
Michael M. <mm@mm-ul.xyz>
2024-01-11 10:25:20 +0000

    Another change

--- highandtidy/settings.py
+++ highandtidy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
         'NAME': 'djangodev20240108',
         'USER': 'michael',
         'PASSWORD': 'michael',
-        'HOST': os.getenv('DATABASE_HOST', '')
+        'HOST': os.getenv('DATABASE_HOST', ''),
         'PORT': '5432'
     }
 }
