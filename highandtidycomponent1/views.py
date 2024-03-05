72ad51d2ce4c11bea414168c2d23af3df94bac10
Michael M. <mm@mm-ul.xyz>
2024-03-05 20:09:59 +0000

    Some changes

--- highandtidycomponent1/views.py
+++ highandtidycomponent1/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def sayhello(request):
     data = {'name': 'Michael'}
     return render(request, 'hello.html', data)
+
+def guest(request):
+    return render(request, 'guest.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
