bf0047b96a3312e4eb57fc66f46e93c9df37dede
michael-muller-tech <github@mm-ul.xyz>
2024-10-01 21:22:32 +0100

    Starting the authentication process. Removed superluous user table in favour of using built-in auth_users table. Removed references to these everywhere.

--- highandtidycomponent1/admin.py
+++ highandtidycomponent1/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Households, Users, Tasks, Assignment
+from .models import Households, Tasks, Assignment
 
 # Register your models here.
 admin.site.register(Households)
-admin.site.register(Users)
 admin.site.register(Tasks)
 admin.site.register(Assignment)
 
--- highandtidycomponent1/models.py
+++ highandtidycomponent1/models.py
@@ -5,23 +5,6 @@ class Households(models.Model):
     household_id = models.AutoField(primary_key=True)
     household_name = models.CharField(max_length=45, default=0)
 
-class Users(models.Model):
-    firstname = models.CharField(max_length=45)
-    lastname = models.CharField(max_length=45)
-    email = models.EmailField(max_length=100)
-    # Use AutoField as primary key
-    userid = models.AutoField(primary_key=True)
-    description = models.TextField(null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now=True)
-    tasks_completed = models.IntegerField(default=0)
-    tasks_created = models.IntegerField(default=0)
-    is_admin = models.BooleanField(default=False)
-    household_id = models.ForeignKey(Households, on_delete=models.CASCADE, default=False)
-
-    def __str__(self):
-        return f"{self.firstname} {self.lastname}"
-
 class Tasks(models.Model):
     # Use AutoField as primary key
     taskid = models.AutoField(primary_key=True)
@@ -36,7 +19,7 @@ class Tasks(models.Model):
     
 class Assignment(models.Model):
     assignment_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
     household = models.ForeignKey(Households, on_delete = models.CASCADE)
     assignment_name = models.CharField(max_length=45)
